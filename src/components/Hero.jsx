64289653e1da24df264b9b9a5c2a31ed6c3ca9e5
Vanreuth <hengvanreuth.17@gmail.com>
2025-05-30 14:57:21 +0700

    portfolio

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,8 +22,6 @@ const Hero = () => {
             className='w-32 h-32 rounded-full object-cover border-4 border-[#915eff]'
           />
         </motion.div>
-
-        {/* 💬 Text Section */}
         <div className='flex flex-row items-start gap-5'>
           <div className='flex flex-col justify-center items-center mt-5'>
             <div className='w-5 h-5 rounded-full bg-[#915eff]' />
@@ -46,6 +44,18 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{ y: [0, 24, 0] }}
+              transition={{ duration: 1.5, repeat: Infinity, repeatType: 'loop' }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+
+      </div>
     </section>
   )
 }
